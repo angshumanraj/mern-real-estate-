381a4ace58c0a1ca6926a12f96cd2889e0935bcf
angshumanraj <angshuman200998@gmail.com>
2024-01-17 13:29:03 +0530

    Connecting to database

--- server/index.js
+++ server/index.js
@@ -1,6 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
+
+mongoose.connect(process.env.MONGO_URL)
+.then(()=>{
+    console.log("database is connected");
+
+})
+.catch((err)=>{
+    console.log(err);
+})
 
 app.listen(3005,()=>{
     console.log("server is listening to port 3005")
