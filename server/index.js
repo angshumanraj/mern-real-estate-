2bb20fbe56d8f5300b88bc7c591ebb8efa2dce59
angshumanraj <angshuman200998@gmail.com>
2024-01-17 20:45:04 +0530

    Implemented a secure signup API route incorporating password encryption for enhanced security.

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/userRoute.js';
-
+import authRouter from './routes/authRoute.js';
 dotenv.config();
 
 const app = express();
@@ -15,9 +15,11 @@ mongoose.connect(process.env.MONGO_URL)
 .catch((err)=>{
     console.log(err);
 })
+app.use(express.json())
 
 app.listen(3005,()=>{
     console.log("server is listening to port 3005")
 });
 
 app.use('/api/user',userRouter);
+app.use('/api/auth',authRouter);
