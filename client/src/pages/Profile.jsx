35a368152a24a3737454b8bff7a6bb1c3a22ca1a
angshumanraj <angshuman200998@gmail.com>
2024-02-22 19:23:22 +0530

    - Added functionality in the Profile component to dispatch an action to update the Redux state with the new avatar URL after successful upload. - Imported the necessary action creator from the user slice and dispatched the action with the updated avatar URL.

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,8 +1,10 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useRef, useState } from "react";
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
 import { app } from "../firebase";
+import { signInSuccess } from "../redux/user/userSlice";
 const Profile = () => {
+  const dispatch = useDispatch();
   const { currentUser } = useSelector((state) => state.user);
   const fileRef=useRef(null);
   const [file,setFile]=useState(undefined);
@@ -21,6 +23,7 @@ const Profile = () => {
   useEffect(()=>{
     if(file){
       handleFileUpload(file);
+    
     }
   },[file]);
 
@@ -29,59 +32,68 @@ const Profile = () => {
     const fileName = new Date().getTime() + '_' + file.name;
     const storageRef = ref(storage, fileName);
     const uploadTask = uploadBytesResumable(storageRef, file);
-      
-    uploadTask.on('state_changed',
-        (snapshot) => {
-            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-            setFilePercentage(Math.round(progress))
-            if (snapshot.state === 'success') {
-                
-              console.log('Upload complete');
-            }
-        },
-        (error) => {
-            // Handle errors here
-            setFileUploadErrro(true)
-            console.error('Upload error:', error);
-        },
-    
-    ()=>{
-      getDownloadURL(uploadTask.snapshot.ref).then(
-        (downloadUrl)=>{
-          setFormData({...formData,avatar:downloadUrl})
-          console.log("file available at ", downloadUrl)
-        }
-      )
-    }
-  );
-};
-
+  
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log('Bytes transferred:', snapshot.bytesTransferred);
+        console.log('Total bytes:', snapshot.totalBytes);
+        console.log('Progress:', progress);
+        setFilePercentage(Math.round(progress));
+        
+      },
+      (error) => {
+        // Handle errors here
+        setFileUploadErrro(true);
+        console.error('Upload error:', error);
+      },
+      () => {
+        
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadUrl) => {
+          setFormData({ ...formData, avatar: downloadUrl });
+          dispatch(signInSuccess({ ...currentUser, avatar: downloadUrl }));
+          console.log('file available at ', downloadUrl);
+        });
+      }
+    );
+  };
+  
 
   return (
     <div>
       <h1 className="text-3xl font-semibold my-7"></h1>
       <form className="max-w-md mx-auto flex flex-col gap-3 items-center">
-        <input onChange={(e)=>setFile(e.target.files[0])} type="file" ref={fileRef} hidden accept="image/*" ></input>
+        <input onChange={(e)=>setFile(e.target.files[0])} 
+                type="file" ref={fileRef} 
+                hidden accept="image/*" >  
+        </input>
         <img
           onClick={()=>fileRef.current.click()}
-          src={currentUser.avatar}
+          src={formData.avatar|| currentUser.avatar}
           alt="profile"
           className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
         ></img>
-        <p>
-          {fileUploadError ? (
-            <span className="text-red-700">Error: Image Upload (image must be less than 2)</span>
-          ) : (
-            filePercentage > 0 && filePercentage < 100 ? (
-              <span className="text-green-700">{`Uploading ${filePercentage}%`}</span>
-            ) : (
-              filePercentage === 100 ? (
-                <span className="text-green-700">Image successfully uploaded</span>
-              ) : ""
-            )
-          )}
-        </p>
+        <p className='text-sm self-center'>
+        {fileUploadError ? (
+          <span className='text-red-700'>
+            Error Image upload (image must be less than 2 mb)
+          </span>
+        ) : filePercentage > 0 && filePercentage < 100 ? (
+          <span className='text-slate-700'>{`Uploading ${filePercentage}%`}</span>
+        ) : filePercentage === 100 ? (
+          <span className='text-green-700'>Image successfully uploaded!</span>
+        ) : (
+          ''
+        )}
+      </p>
 
+        {/* progress bar */}
+        <div className="w-full bg-gray-200 rounded-lg h-4 relative">
+        <div className="bg-green-500 absolute top-0 left-0 h-full" 
+             style={{ width: `${filePercentage}%` }}>
+        </div>
+      </div>
 
         <input
           type="text"
