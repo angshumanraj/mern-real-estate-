5de7f640a0e9405ff04a663ace1e2b7deb9d20d8
angshumanraj <angshuman200998@gmail.com>
2024-03-08 16:25:26 +0530

    incorrect varible name

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -67,7 +67,7 @@ export const google=async(req,res,next)=>{
                 avatar:req.body.photo
             });
             await newUser.save();
-            const token=jwt.sign({id:user._id},process.env.JWT_SECRET);
+            const token=jwt.sign({id:newUser._id},process.env.JWT_SECRET);
             const {password:pass,...rest} =user._doc
             res
             .cookie('access_token',token,{httpOnly:true})
