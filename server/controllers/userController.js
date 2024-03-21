8327d3023a2883aaa75c668d3b87e0f3043f00d4
angshumanraj <angshuman200998@gmail.com>
2024-03-21 15:34:43 +0530

    peveious

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -68,3 +68,20 @@ export const getUserListing=async(req,res,next)=>{
         return next(errorHandler(401,'you can only view your own listings!'))
     }
 }
+
+export const deleteListing=async(req,res,next)=>{
+    const listing=await Listing.findById(req.params.id)
+    if(!listing){
+        return next(errorHandler(404,'Listing not found!'))
+    }     
+
+    if(req.user.id!==listing.userRef){
+        return next(errorHandler(401,'You can delete your own listing'))
+    }
+    try {
+        await Listing.findByIdAndDelete(req.params.id);
+        res.status(200).json("Listing is deleted")
+    } catch (error) {
+        next(error)
+    } 
+}
--- server/routes/listingRoute.js
+++ server/routes/listingRoute.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { createListing } from "../controllers/listingController.js";
 import {verifyUser} from "../utils/verifyUser.js"
+import { deleteListing } from "../controllers/userController.js";
 
 const router= express.Router();
 
 router.post('/create',verifyUser,createListing);
+router.delete('/delete/:id', verifyUser, deleteListing);
 
 export default router;
